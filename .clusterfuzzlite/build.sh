57e5d037aa69c6c37ae2573556b109891c1488e1
tzcnt <trailingzerocount@gmail.com>
2025-12-18 08:40:11 -0800

    address code review issues

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -4,6 +4,7 @@ $CC $CFLAGS -I include \
     src/lib/zxc_common.c \
     src/lib/zxc_compress.c \
     src/lib/zxc_decompress.c \
+    src/lib/zxc_driver.c \
     tests/fuzz.c \
     -o $OUT/zxc_fuzzer \
     $LIB_FUZZING_ENGINE \
