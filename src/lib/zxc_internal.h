5ce0d596e2d017381211f57b13cfb81960df1a77
hellobertrand <5901952+hellobertrand@users.noreply.github.com>
2026-01-08 15:11:54 +0100

    Fixes undefined behavior in bit twiddling
    Replaces potentially undefined behavior in count trailing zeros implementations with well-defined unsigned arithmetic.
    This ensures consistent and predictable results across different compilers and platforms.

--- src/lib/zxc_internal.h
+++ src/lib/zxc_internal.h
@@ -509,7 +509,7 @@ static ZXC_ALWAYS_INLINE int zxc_ctz32(uint32_t x) {
     static const int DeBruijn32[32] = {0,  1,  28, 2,  29, 14, 24, 3,  30, 22, 20,
                                        15, 25, 17, 4,  8,  31, 27, 13, 23, 21, 19,
                                        16, 7,  26, 12, 18, 6,  11, 5,  10, 9};
-    return DeBruijn32[((uint32_t)((x & -((int)x)) * 0x077CB531U)) >> 27];
+    return DeBruijn32[((uint32_t)((x & (0U - x)) * 0x077CB531U)) >> 27];
 #endif
 }
 
@@ -540,7 +540,7 @@ static ZXC_ALWAYS_INLINE int zxc_ctz64(uint64_t x) {
         0,  1,  48, 2,  57, 49, 28, 3,  61, 58, 50, 42, 38, 29, 17, 4,  62, 55, 59, 36, 53, 51,
         43, 22, 45, 39, 33, 30, 24, 18, 12, 5,  63, 47, 56, 27, 60, 41, 37, 16, 54, 35, 52, 21,
         44, 32, 23, 11, 46, 26, 40, 15, 34, 20, 31, 10, 25, 14, 19, 9,  13, 8,  7,  6};
-    return Debruijn64[((x & -x) * 0x03F79D71B4CA8B09ULL) >> 58];
+    return Debruijn64[((x & (0ULL - x)) * 0x03F79D71B4CA8B09ULL) >> 58];
 #endif
 }
 
